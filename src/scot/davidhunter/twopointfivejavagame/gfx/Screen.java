ce6648e87757fc87d3df0aefb5065820a0af52da
DavidHunterScot <contact@davidhunter.scot>
2022-11-11 19:53:48 +0000

    DesignsByZephyr Episode 6 - Mirroring the Sprites

--- src/scot/davidhunter/twopointfivejavagame/gfx/Screen.java
+++ src/scot/davidhunter/twopointfivejavagame/gfx/Screen.java
@@ -24,7 +24,7 @@ public class Screen
 		pixels = new int[ width * height ];
 	}
 	
-	public void render( int xPos, int yPos, int tile, int colour )
+	public void render( int xPos, int yPos, int tile, int colour, boolean mirrorX, boolean mirrorY )
 	{
 		xPos -= xOffset;
 		yPos -= yOffset;
@@ -40,12 +40,19 @@ public class Screen
 			
 			int ySheet = y;
 			
+			if ( mirrorY )
+				ySheet = 7 - y;
+			
 			for ( int x = 0; x < 8; x++ )
 			{
 				if ( x + xPos < 0 || x + xPos >= width )
 					continue;
 				
 				int xSheet = x;
+				
+				if ( mirrorX )
+					xSheet = 7 - x;
+				
 				int col = ( colour >> ( sheet.pixels[ xSheet + ySheet * sheet.width + tileOffset ] * 8 ) & 255 );
 				
 				if ( col < 255 )
